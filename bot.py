8217de2de7c8e4afe0d84acb36a9771ee87f1551
JOramas <javiale2000@gmail.com>
2021-08-11 03:59:05 +0000

    scheduler and delete

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from pyrogram import Client, filters
 from read_config import read_config
 import json
 import requests
+import schedule
+import time
 
 def read_data(id):
     id = str(id)
@@ -25,9 +27,8 @@ app = Client(config_data['bot_user_name'], config_data['api_id'], config_data['a
 @app.on_message(filters.command('add'))
 def add_scholar(client, message):
     users = message.text.split()
-    name = str(users[-3])
-    wallet = str(users[-2])
-    slp = str(users[-1])
+    name = str(users[-2])
+    wallet = str(users[-1])
     
     os.makedirs("./db", exist_ok=True)
     db = read_data(message.chat.id)
@@ -36,10 +37,28 @@ def add_scholar(client, message):
     if not name in db:
         db[name] = {
                     "wallet": wallet,
-                    "slp":slp
+                    "slp": "0"
                     }
         write_data(message.chat.id,db)
+    else:
+        message.reply_text("Ya tienes un scholar con ese nombre")
+    pass
 
+@app.on_message(filters.command('del'))
+def del_scholar(client, message):
+    users = message.text.split()
+    name = str(users[-2])
+    # wallet = str(users[-1])
+    
+    os.makedirs("./db", exist_ok=True)
+    db = read_data(message.chat.id)
+    # db = read_data('1')
+ 
+    if name in db:
+        db.pop(name)
+        write_data(message.chat.id,db)
+    else:
+        message.reply_text("no tienes un scholar con ese nombre")
     pass
 
 @app.on_message(filters.command('standing'))
@@ -55,6 +74,7 @@ def see_fee(client, message):
             wallet = db[i]['wallet']
             slp = requests.get(f'https://game-api.skymavis.com/game-api/clients/{wallet}/items/1').json()['total']
             list.append((i,slp))
+            
         list.sort(key=lambda x:x[1], reverse=True)
         stand = ''
         for i in list:
@@ -64,13 +84,51 @@ def see_fee(client, message):
         message.reply_text('no tienes scholars :(')
     pass
 
+# @app.on_message(filters.command('help'))
+def get_snapshot():
+    
+    for cp,dir,files in os.walk('./db/'):
+        for file in files:
+            if file.endswith('.json'):
+                user = file[:len('.json')]
+                db = read_data(user)
+        
+                if len(db.keys()) > 0:
+                    for i in db.keys():
+                        wallet = db[i]['wallet']
+                        slp = requests.get(f'https://game-api.skymavis.com/game-api/clients/{wallet}/items/1').json()['total']
+                        list.append((i,slp))
+            
+                    list.sort(key=lambda x:x[1], reverse=True)
+                    stand = 'Weekly Snapshot:\n'
+                    
+                    for i in list:
+                        stand += f'{i[0]} : {i[1]}\n'
+                        
+                    app.send_message(user,stand)
+                # else:
+                    # message.reply_text('no tienes scholars :(')
+                pass  
+
+def scheduler_snapshot():
+    from apscheduler.schedulers.background import BackgroundScheduler
+    
+    sched = BackgroundScheduler()
+    sched.add_job(get_snapshot, 'cron', day_of_week='sun', hour=8, minute=0)
+    sched.start()
+
 @app.on_message(filters.command('help'))
 @app.on_message(filters.command('start'))
 def help(client, message):
     
     message.reply_text("""
-Puedes contribuir con el desarrollo aqui: https://github.com/JavierOramas/scholar_tracker_bot\no puedes donar para contribuir al desarrollo: 0x64eF391bb5Feae6023440AD12a9870062dd2B342
-""")
+    /add nombre wallet - añade el usuario a tu lista de scholars, recuerda sustituir ronin: por 0x\n
+    /del nombre - elimina el usuario de tu lista\n
+    /standing - muestra todos los scholars ordenados\n
+    Puedes contribuir con el desarrollo aqui: https://github.com/JavierOramas/scholar_standing_bot\no puedes donar para contribuir al desarrollo: 0x64eF391bb5Feae6023440AD12a9870062dd2B342
+    """)
     pass
 
+
+scheduler_snapshot()
 app.run()
